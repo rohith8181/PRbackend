e12cc7a353bb728a530e486c55285976d221e98a
ROHITH <rohithperala81@gmail.com>
2023-07-26 20:35:01 +0530

    verify mail updated

--- requests/Login.js
+++ requests/Login.js
@@ -30,7 +30,7 @@ router.post('/request/login', async (req, res) => {
                 const token = generatetoken(email);
                 user.verificationToken = token;
                 await user.save();
-                const link = `http://localhost:3000/verify/${token}`;
+                const link = `https://64c133dd85496f104015610b--euphonious-melomakarona-254aa9.netlify.app/verify/${token}`;
                 sendMail(email, link);
 
                 return res.json({ status: 201, message: "Email not Veified" });
